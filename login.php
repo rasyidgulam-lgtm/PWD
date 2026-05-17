5587f1544ec3d72ff5c5c7636848449d5f8b1eb1
Rizal Fattah <icalpattah@gmail.com>
2026-05-17 23:57:23 +0700

    tampilan cantik

--- login.php
+++ login.php
@@ -34,6 +34,7 @@ if(isset($_POST['login'])){
 <head>
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body class="bg-light">
     <div class="container">
